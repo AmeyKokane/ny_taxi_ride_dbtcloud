6e84eccd8b3a562eaffaa27b949a9b816b5bcd0e
Amey Kokane <amey.kokane@gmail.com>
2022-02-20 03:39:44 +0000

    second commit-all models finally running

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -32,7 +32,7 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(0 as integer) as ehail_fee,
+    --cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
